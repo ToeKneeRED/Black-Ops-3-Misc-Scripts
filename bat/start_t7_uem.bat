7dacbc8c9177ce961ef4bb4a71d13415ee2e7448
Tony <toekneegaming123@gmail.com>
2024-09-20 00:10:34 -0400

    add: multiple argument support
    allows the use of starting bat via non-steam game in steam library

--- bat/start_t7_uem.bat
+++ bat/start_t7_uem.bat
@@ -2,57 +2,87 @@
 cls
 title Toe Knee UEM t7 bat
 
-set t7Path=""
+set t7Path=
 set steamPath=""
+set argCount=0
+set argVec=
+set debugMode=
 
+:START
 if "%~1"=="" (
-	echo Searching for t7patch...
-	for %%i in ("%~dp0t7patch_*.exe") do (
-		set t7Path="%%~i"
-		goto :T7_FOUND
-	)
-	
-	if %t7Path%=="" (
-        goto T7_NOT_FOUND
-    )
+	goto T7_SEARCH
 ) else (
-		set t7Path="%~1"
-		if t7Path=="" goto T7_NOT_FOUND
+	goto HANDLE_ARGUMENTS
 )
+
+:HANDLE_ARGUMENTS
+for %%x in (%*) do (
+   set /A argCount+=1
+   set "argVec[!argCount!]=%%~x"
+   
+   if "%%~x"=="-debug" (
+	set "debugMode=1"
+   ) else (
+	set "t7Path=%%~x"
+   )
+)
+if not defined t7Path goto T7_NOT_FOUND
+goto T7_SEARCH
+
+:T7_SEARCH
+echo Searching for t7patch...
+for %%i in ("%~dp0t7patch_*.exe") do (
+	set "t7Path=%%~i"
+)
+if not defined t7Path (
+	goto T7_NOT_FOUND
+)
+goto T7_FOUND
 
 :T7_FOUND
 echo t7patch path: %t7Path%
 
-:T7START
-if t7Path=="" ( 
+:T7_START
+if not defined t7Path ( 
 	goto T7_NOT_FOUND
 )
-start "" %t7Path%
+start "" "%t7Path%"
 timeout 1 >nul
-
-echo Searching for steam...
-for /f "tokens=1,3*" %%E in ('reg query "HKEY_CURRENT_USER\Software\Valve\Steam"') do (
-    if %%E==SteamExe (
-		if "%%G"=="" (
-			set steamPath="%%F"
-		) else (
-			set steamPath="%%F %%G"
-		)
-	)
-)
-
-echo Steam path: %steamPath%
-
-if %steamPath%=="" ( 
-	goto STEAM_NOT_FOUND
-) else  (
- 	%steamPath% -applaunch 311210 +set fs_game 2942053577
-)
-goto END
+goto STEAM_SEARCH
 
 :T7_NOT_FOUND
 echo t7patch executable was not found
 pause
+goto END
+
+:STEAM_SEARCH
+echo Searching for steam...
+for /f "tokens=1,3*" %%E in ('reg query "HKEY_CURRENT_USER\Software\Valve\Steam"') do (
+    if %%E==SteamExe (
+		set "steamPath=%%E"
+		REM if "%%G"=="" (
+			REM set steamPath="%%F"
+		REM ) else (
+			REM set steamPath="%%F %%G"
+		REM )
+		goto STEAM_FOUND
+	)
+)
+
+:STEAM_FOUND
+echo Steam path: %steamPath%
+
+:STEAM_START
+if %steamPath%=="" ( 
+	goto STEAM_NOT_FOUND
+) else  (
+	if defined debugMode (
+		echo would launch game
+		pause
+	) else (
+		%steamPath% -applaunch 311210 +set fs_game 2942053577
+	)
+)
 goto END
 
 :STEAM_NOT_FOUND
